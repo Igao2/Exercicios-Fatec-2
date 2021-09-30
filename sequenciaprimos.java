ec51d1828663ea3ee414ede431c66d135fa11abe
Igao2 <91141990+Igao2@users.noreply.github.com>
2021-09-30 19:12:48 -0300

    Update sequenciaprimos.java

--- sequenciaprimos.java
+++ sequenciaprimos.java
@@ -1,4 +1,23 @@
-
+import java.util.Scanner;
 public class sequenciaprimos {
-
+	public static void main(String[]args) {
+		int v;
+		int i;
+		int x;
+		Scanner s = new Scanner(System.in);
+		System.out.println("Digite o valor para definir o começo da sequência: ");
+		x = s.nextInt();
+		System.out.println("Digite o valor para definir o final da sequência: ");
+		v = s.nextInt();
+		 	for(i=x;i<=v;i++) {
+		 		int contador=0;
+		 			for(int d=1;d<=i;d++) {
+		 				if(i%d==0) {
+		 					contador++;
+		 				}
+		 			}if(contador==2) {
+		 				System.out.println("Esse número é primo: "+i);
+		 			}
+		 	}
+}
 }
